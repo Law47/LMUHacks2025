dedac3d3b67f8b9c16769a57ae57d23da85d278c
zleodai <awsomeleodai@gmail.com>
2025-03-15 17:50:30 -0700

    fixing merge conflict

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,6 +15,8 @@ public class PlayerController : MonoBehaviour
     public UnityEvent walkingRight;
     public UnityEvent walkingLeft;
     public UnityEvent notWalking;
+    public UnityEvent changeKeys;
+    public GameObject changeKeyColliderObj;
 
     void Start()
     {
@@ -59,7 +61,7 @@ public class PlayerController : MonoBehaviour
         rb.linearVelocity = new Vector2(moveDirection * moveSpeed, rb.linearVelocity.y);
     }
 
-    public void SetMovementKeys(KeyCode newLeftKey, KeyCode newRightKey)
+    public void SetMovementKeys(KeyCode newLeftKey, KeyCode newRightKey, GameObject buttonObject)
     {
         if (newLeftKey != KeyCode.None)
         {
@@ -69,5 +71,7 @@ public class PlayerController : MonoBehaviour
         {
             moveRightKey = newRightKey;
         }
+        changeKeyColliderObj = buttonObject;
+        changeKeys.Invoke();
     }
 }
